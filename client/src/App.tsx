4094bfcc5b6d719a94f665f432e8ef87b4691892
yaelBrown <yaelrbrown@gmail.com>
2022-10-20 19:43:14 -0500

    add: add client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AudioPlayer from './Components/AudioPlayer/AudioPlayer'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>audio player</h1>
+      <AudioPlayer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AudioPlayer(props: any) {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
